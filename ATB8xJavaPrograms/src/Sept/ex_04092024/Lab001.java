892730d3441bcbc7dbb54bcadbbf402e67d5e103
VindyaKoppad <vindyank22@gmail.com>
2024-11-11 19:21:35 +0530

    Initial Commit

---
+++ ATB8xJavaPrograms/src/Sept/ex_04092024/Lab001.java
@@ -0,0 +1,10 @@
+package Sept.ex_04092024;
+
+public class Lab001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
---
+++ ATB8xJavaPrograms/src/Sept/ex_04092024/Lab002.java
@@ -0,0 +1,11 @@
+package Sept.ex_04092024;
+
+public class Lab002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
