3f0de5c65e74947be91278a12a1e3ceaa1f7144d
VindyaKoppad <vindyank22@gmail.com>
2024-11-17 20:18:52 +0530

    Variables in java

--- ATB8xJavaPrograms/src/Sept/ex_11092024/Lab012.java
+++ ATB8xJavaPrograms/src/Sept/ex_11092024/Lab012.java
@@ -4,6 +4,10 @@ public class Lab012 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//data type variable_name = variable value
+		//boolean is_QA=True; - (true not True)
+		boolean is_QA=true;
+		
 
 	}
 
---
+++ ATB8xJavaPrograms/src/Sept/ex_11092024/Lab013.java
@@ -0,0 +1,24 @@
+package Sept.ex_11092024;
+
+public class Lab013 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Variable(Identifier) - Rules we need to follow 
+		//boolean 123=true;
+		boolean $=true;
+		boolean vindya_is_QA=true;
+		//bollean 123vindya=true;
+		boolean vindya123=true;
+		boolean _123=true;
+		//boolean 123_=true;
+		System.out.println(_123);
+		boolean VINDYA=true;
+		//boolean vindya qa=true;
+		boolean vindya$qa=true;
+		boolean vindya_qa=true;
+		
+
+	}
+
+}
