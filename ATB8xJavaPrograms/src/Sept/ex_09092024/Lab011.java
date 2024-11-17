07d5319a31cfb8678e84e4f926f8f6792c92bbd9
VindyaKoppad <vindyank22@gmail.com>
2024-11-17 19:43:16 +0530

    Keywords in java

---
+++ ATB8xJavaPrograms/src/Sept/ex_09092024/Lab011.java
@@ -0,0 +1,15 @@
+package Sept.ex_09092024;
+
+public class Lab011 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Variables, Data Types, Identifiers
+		boolean do_you_have_audi= false;
+		boolean is_this_java_program = true;
+		
+				
+
+	}
+
+}
---
+++ ATB8xJavaPrograms/src/Sept/ex_11092024/Lab012.java
@@ -0,0 +1,10 @@
+package Sept.ex_11092024;
+
+public class Lab012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
