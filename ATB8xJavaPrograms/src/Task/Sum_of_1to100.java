fbe747168e0a7f0c3a7da42d060ee853164741f3
VindyaKoppad <vindyank22@gmail.com>
2025-02-27 13:31:39 +0530

    Tasks

---
+++ ATB8xJavaPrograms/src/Task/Sum_of_1to100.java
@@ -0,0 +1,17 @@
+package Task;
+ 
+public class Sum_of_1to100 {
+
+	public static void main(String[] args) {
+		//Sum of numbers from 1 to 100 using while loop
+		
+		int sum=0;
+		int i=0;
+		while(i<=100){
+			sum=sum+i;
+			i++;
+			
+		}System.out.println(sum);
+		
+	}
+}
